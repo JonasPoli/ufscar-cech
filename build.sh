c297f97bff7280ed55e1a053ccb4dbaa43379471
Jonas Poli <jonas@wab.com.br>
2026-08-27 15:36:41 -0300

    chore: update PHP binary auto-detection logic to prioritize PHP 8.4 paths

--- build.sh
+++ build.sh
@@ -3,14 +3,18 @@ set -e
 
 # Auto-detect PHP binary if not specified
 if [ -z "$PHP_BIN" ]; then
-    if [ -x "/RunCloud/Packages/RunCloud-PHP83rc/bin/php" ]; then
-        PHP_BIN="/RunCloud/Packages/RunCloud-PHP83rc/bin/php"
+    if [ -x "/RunCloud/Packages/php84rc/bin/php" ]; then
+        PHP_BIN="/RunCloud/Packages/php84rc/bin/php"
+    elif [ -x "/RunCloud/Packages/php83rc/bin/php" ]; then
+        PHP_BIN="/RunCloud/Packages/php83rc/bin/php"
     elif [ -x "/RunCloud/Packages/RunCloud-PHP84rc/bin/php" ]; then
         PHP_BIN="/RunCloud/Packages/RunCloud-PHP84rc/bin/php"
-    elif command -v php8.3 &> /dev/null; then
-        PHP_BIN="php8.3"
+    elif [ -x "/RunCloud/Packages/RunCloud-PHP83rc/bin/php" ]; then
+        PHP_BIN="/RunCloud/Packages/RunCloud-PHP83rc/bin/php"
     elif command -v php8.4 &> /dev/null; then
         PHP_BIN="php8.4"
+    elif command -v php8.3 &> /dev/null; then
+        PHP_BIN="php8.3"
     else
         PHP_BIN="php"
     fi
